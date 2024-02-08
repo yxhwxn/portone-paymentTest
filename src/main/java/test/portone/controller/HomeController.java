52f07b729aa6089993c2f257c2015385cd46dfe4
yxhwxn <officeyh9819@gmail.com>
2024-02-08 18:21:11 +0900

    :sparkles: Feat: KG이니시스 결제 추가

--- src/main/java/test/portone/controller/HomeController.java
+++ src/main/java/test/portone/controller/HomeController.java
@@ -10,6 +10,12 @@ public class HomeController {
 
     @GetMapping("/payment")
     public Mono<Rendering> payment(){
-        return Mono.just(Rendering.view("payment").build());
+
+        return Mono.just(Rendering.view("payment1").build());
+    }
+    @GetMapping("/payment2")
+    public Mono<Rendering> payment2(){
+
+        return Mono.just(Rendering.view("payment2").build());
     }
 }
