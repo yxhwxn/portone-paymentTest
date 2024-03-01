74fc89908dde0b1a249549284ff6b5f738456318
Yuhwan Hwang <87745916+yxhwxn@users.noreply.github.com>
2024-03-01 21:58:53 +0900

    Remove PaymentRequestDto.java

--- src/main/java/test/portone/controller/PaymentRequestDto.java
+++ src/main/java/test/portone/controller/PaymentRequestDto.java
@@ -1,16 +1,1 @@
-package test.portone.controller;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class PaymentRequestDto {
-
-    @JsonProperty
-    private String impUid;
-
-    @JsonProperty
-    private String merchantUid;
-}
